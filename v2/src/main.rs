02ded926180ba35f2842d4c4d3d9c8300e769a0b
JuanD <dosquisd123@gmail.com>
2025-08-05 21:39:38 -0500

    feat(v2): dockerize 🤓

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(), std::io::Error> {
             .wrap(cors)
             .wrap(Logger::default())
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
     /*
